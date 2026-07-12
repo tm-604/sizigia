35c33e53b1377ca343d3f3eeaa54ff23873cac16
tm-604 <2psilo@gmail.com>
2026-07-13 00:03:59 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Bump this whenever index.html (or the lineup data inside it) changes,
 // so returning visitors pick up the new version instead of a stale cache.
-const CACHE_NAME = 'sizigia-cache-v51';
+const CACHE_NAME = 'sizigia-cache-v52';
 const ASSETS = [
   './',
   './index.html',
